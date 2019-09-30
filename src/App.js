9c3098e4d181d68e7dfee5cf2615b5e860325252
HUANG Meng <mark.huang1212@gmail.com>
2019-09-30 16:42:32 +0800

    ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+    const [searchMode, setSearchMode] = useState('quick')
+    const switchMode = () => {
+        setSearchMode(searchMode == 'quick' ? 'advanced' : 'quick')
+    }
+
+    return (
+        <div className="App">
+            <div className="AppBar">
+                <div style={{ marginLeft: '24px' }}><span>Find Free Classroom&nbsp;</span>@ UST</div>
+            </div>
+            <div className="AppContent">
+                <div className="AppModeSelector">
+                    <span onClick={() => switchMode()}
+                        className={
+                            searchMode == 'quick' ? 'current' : 'non-current'
+                        }>Quick</span>&nbsp;|&nbsp;
+                    <span onClick={() => switchMode()}
+                        className={
+                            searchMode == 'advanced' ? 'current' : 'non-current'
+                        }>Advanced</span>
+                </div>
+                <div className="AppSearchBox">
+                    <input></input>
+                    <i className="material-icons">arrow_forward</i>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
