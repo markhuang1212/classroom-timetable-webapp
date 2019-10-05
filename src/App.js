7e8c5d999730eb6c10eb924ccf69a5e9c689130b
HUANG Meng <mark.huang1212@gmail.com>
2019-10-05 21:41:31 +0800

    de

--- src/App.js
+++ src/App.js
@@ -5,32 +5,20 @@ import './App.css';
 function App() {
     const [inputText, setInputText] = useState('')
     const [results, setResults] = useState([])
-    const [numOfFree,setNumOfFree]=useState(0)
+    const [numOfFree, setNumOfFree] = useState(0)
     const searchInput = useRef(null)
     const searchButton = useRef(null)
-
-    const onSearch = () => {
-        console.log(`Search: ${inputText}`)
-        setResults(dataFilter().map(v => {
-            return {
-                // room: v.split(',')[0].replace('Lecture Theater ', 'LT').split(' (')[0],
-                room: v.split(', ')[0].split(' (')[0].replace('Lecture Theater ', 'LT'),
-                desc: v.split(', ')[1] ? v.split(', ')[1].split(' (')[0] : '',
-                until: 'Unknown'
-            }
-        }))
-    }
+    const noticeBox = useRef(null)
 
     useEffect(() => {
-        searchInput.current.addEventListener('keyup', e => {
-            if (e.keyCode === 13) {
-                e.preventDefault()
-                searchButton.current.click()
-            }
-        })
+
+        const day = (new Date()).getDay()
+        if (day == 6 || day == 7) {
+            noticeBox.current.style.display = 'block'
+        }
 
         const filteredData = dataFilter()
-        setNumOfFree(filteredData.length)
+        setNumOfFree(filteredData.filter(v => v.room != 'TBA').length)
         setResults(filteredData.map(v => {
             return {
                 // room: v.split(',')[0].replace('Lecture Theater ', 'LT').split(' (')[0],
@@ -48,34 +36,41 @@ function App() {
             </div>
             <div className="AppContent">
                 <div className="scrollable">
-                    <div className="AppNoticeBox">
-                        {numOfFree} Classrooms <br/>
-                        is free.
+                    <div className="AppNoticeCont">
+                        <div ref={noticeBox} className="AppIntroNotice">
+                            Don't use this app on weekends since all classrooms are locked.
+                        </div>
+                        <div className="AppClassroomNotice">
+                            {numOfFree} Classrooms <br />
+                            are currently free.
+                        </div>
                     </div>
                     <div className="AppSearchBox">
-                        <input ref={searchInput} 
-                            value={inputText} 
+                        <input ref={searchInput}
+                            value={inputText}
                             onChange={e => setInputText(e.target.value)}
-                            placeholder="Search by Lift/Building"></input>
-                        <i className="material-icons" ref={searchButton} onClick={() => onSearch()}>arrow_forward</i>
+                            placeholder="Lift/Building/Room"></input>
+                        <i className="material-icons" ref={searchButton}>arrow_forward</i>
                     </div>
                     <div className="AppResult">
-                        {results.map((v, i) => {
-                            let hr = i < results.length - 1 ? (<hr />) : ''
-                            return (
-                                <div className="ResultItem" key={i}>
-                                    <div className="LeftCont">
-                                        <div className="RoomNo">{v.room}</div>
-                                        <div className="RoomDesc">{v.desc}</div>
+                        {results.filter(v => v.room.toUpperCase().includes(inputText.toUpperCase()) || v.desc.toUpperCase().includes(inputText.toUpperCase()))
+                            .filter(v => v.room != 'TBA')
+                            .map((v, i) => {
+                                let hr = i < results.length - 1 ? (<hr />) : ''
+                                return (
+                                    <div className="ResultItem" key={i}>
+                                        <div className="LeftCont">
+                                            <div className="RoomNo">{v.room}</div>
+                                            <div className="RoomDesc">{v.desc}</div>
+                                        </div>
+                                        <div className="FreeUntilCont">
+                                            <div>Free Until:</div>
+                                            <div>{v.until}</div>
+                                        </div>
+                                        <i className="material-icons">keyboard_arrow_right</i>
                                     </div>
-                                    <div className="FreeUntilCont">
-                                        <div>Free Until:</div>
-                                        <div>{v.until}</div>
-                                    </div>
-                                    <i className="material-icons">keyboard_arrow_right</i>
-                                </div>
-                            )
-                        })}
+                                )
+                            })}
 
                     </div>
                 </div>
